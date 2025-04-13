cab24bd3ee6df1179928c487dcbe57938a77a76c
Giorgi Kokolashvili <g.kokolashvili017@gmail.com>
2025-04-13 22:53:52 +0400

    Add home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { FooterPlaceComponent } from "./footer-place/footer-place.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, FooterPlaceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/footer-place/footer-place.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-footer-place',
+  imports: [RouterModule],
+  templateUrl: './footer-place.component.html',
+  styleUrl: './footer-place.component.scss'
+})
+export class FooterPlaceComponent {
+
+}
